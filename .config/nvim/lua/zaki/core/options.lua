9ce929efa2d0910719d595b1c7b0b365e55f32cf
Zakkye on Arch <bouzara.zakaria.25@gmail.com>
2025-02-06 02:43:14 +0100

    nvim keybinding

--- .config/nvim/lua/zaki/core/options.lua
+++ .config/nvim/lua/zaki/core/options.lua
@@ -8,7 +8,6 @@ opt.number = true -- shows absolute line number on cursor line (when relative nu
 
 -- tabs & indentation
 opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 4 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
