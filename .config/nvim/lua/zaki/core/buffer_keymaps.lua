b9b380acc9887be06d3db4d5c33b251f14212b29
Zakkye on Arch <bouzara.zakaria.25@gmail.com>
2025-03-17 01:37:41 +0100

    17-03-2025

--- .config/nvim/lua/zaki/core/buffer_keymaps.lua
+++ .config/nvim/lua/zaki/core/buffer_keymaps.lua
@@ -3,15 +3,14 @@ local keymap = vim.keymap
 -- Buffer navigation
 keymap.set("n", "<space>]", ":BufferLineCycleNext<CR>", { desc = "Go to next buffer" })
 keymap.set("n", "<space>[", ":BufferLineCyclePrev<CR>", { desc = "Go to previous buffer" })
-keymap.set("n", "bx", ":bd<CR>", { desc = "Delete current buffer" })
-keymap.set("n", "bc", ":bdelete<CR>", { desc = "Close current buffer" })
-keymap.set("n", "ba", ":BufferLinePickClose<CR>", { desc = "Close all buffers except current" })
-keymap.set("n", "bn", ":enew<CR>", { desc = "Create a new empty buffer" })
+keymap.set("n", "<space>bx", ":bd<CR>", { desc = "Delete current buffer" })
+keymap.set("n", "<space>bc", ":bdelete<CR>", { desc = "Close current buffer" })
+keymap.set("n", "<space>ba", ":BufferLinePickClose<CR>", { desc = "Close all buffers except current" })
+keymap.set("n", "<space>bn", ":enew<CR>", { desc = "Create a new empty buffer" })
 
 -- Switch to specific buffer by number (1 to 5 as example)
 for i = 1, 5 do
 	keymap.set("n", "<space>" .. i, ":BufferLineGoToBuffer " .. i .. "<CR>", { desc = "Go to buffer " .. i })
-	keymap.set("n", "b" .. i, ":BufferLineGoToBuffer " .. i .. "<CR>", { desc = "Go to buffer " .. i })
 end
 
 -- Move current buffer
