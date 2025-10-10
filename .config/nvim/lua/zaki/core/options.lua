4bba7028327c168fecc3f9a93c869083e0372b58
Zakkye on Arch <bouzara.zakaria.25@gmail.com>
2025-10-10 14:20:45 +0100

    line wrap deactivating

--- .config/nvim/lua/zaki/core/options.lua
+++ .config/nvim/lua/zaki/core/options.lua
@@ -5,6 +5,7 @@ local opt = vim.opt -- for conciseness
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.wrap = false
 
 -- tabs & indentation
 opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
